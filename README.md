096e862cd03efd2840d64fc439eb0900d0505fb6
charlie <chuckiels2011@gmail.com>
2016-01-07 12:09:39 -0500

    added goals to readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # os
-Toy OS. Mostly based on https://littleosbook.github.io
+Minimal OS. What's been implemented:
+
+# Goals
+- <s>Implement an entire working (x86) OS in less lines than systemd.</s> Wait. That's not a challenge at all.
+- Implement an entire working (x86) OS in less lines than 10% the sloc of systemd. There we go.
+    - Current goal for total sloc is 10k (including stdutils)
+- Simpler is *always* better.
+
+# Ideas
+- A super compatability layer. Basically wine baked into the kernel.
+- Microkernel, cause it seems better?
+- Separation of cruft ('cause the world is full of it). If compatablity garbage needs to run, keep it away from everything else.
+
+# What's implemented
+- GDT
+- Interrupt Handling
+    - Timer (PIC)
+    - Keyboard
+
+Also, there's an embedded Lisp for use later. Mostly because I can.
--- system.h
+++ system.h
@@ -10,6 +10,7 @@ struct regs
     unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */
 };
 
+// kmain.c
 extern unsigned char *memcpy(unsigned char *dest, const unsigned char *src, int count);
 extern unsigned char *memset(unsigned char *dest, unsigned char val, int count);
 extern unsigned short *memsetw(unsigned short *dest, unsigned short val, int count);
@@ -17,28 +18,35 @@ extern int strlen(const char *str);
 extern unsigned char inportb (unsigned short _port);
 extern void outportb (unsigned short _port, unsigned char _data);
 
+// scrn.c
 extern void cls();
 extern void putch(unsigned char c);
 extern void puts(unsigned char *str);
 extern void settextcolor(unsigned char forecolor, unsigned char backcolor);
 extern void init_video();
 
+// gdt.c
 void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran);
 void gdt_install(void);
 
+// idt.c
 void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
 void idt_install();
 
+// isrs.c
 void isrs_install();
 
+// irq.c
 void irq_install_handler(int irq, void (*handler)(struct regs *r));
 void irq_uninstall_handler(int irq);
 void irq_install();
 void irq_handler(struct regs *r);
 
+// timer.c
 void timer_install();
 void timer_wait(int ticks);
 
+// kb.c
 void keyboard_install();
 
 #endif
