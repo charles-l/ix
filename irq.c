9e8feb9ca045a776bf43a64d0f89936ae9d04bff
charlie <chuckiels2011@gmail.com>
2016-01-06 21:07:06 -0500

    working keyboard!

--- irq.c
+++ irq.c
@@ -51,20 +51,20 @@ void irq_install()
 {
     irq_remap();
     idt_set_gate(32, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(33, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(34, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(36, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(37, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(38, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(39, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(40, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(41, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(42, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(43, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(44, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(45, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(46, (unsigned) irq0, 0x08, 0x8E);
-    idt_set_gate(47, (unsigned) irq0, 0x08, 0x8E);
+    idt_set_gate(33, (unsigned) irq1, 0x08, 0x8E);
+    idt_set_gate(34, (unsigned) irq2, 0x08, 0x8E);
+    idt_set_gate(36, (unsigned) irq3, 0x08, 0x8E);
+    idt_set_gate(37, (unsigned) irq4, 0x08, 0x8E);
+    idt_set_gate(38, (unsigned) irq5, 0x08, 0x8E);
+    idt_set_gate(39, (unsigned) irq6, 0x08, 0x8E);
+    idt_set_gate(40, (unsigned) irq7, 0x08, 0x8E);
+    idt_set_gate(41, (unsigned) irq8, 0x08, 0x8E);
+    idt_set_gate(42, (unsigned) irq9, 0x08, 0x8E);
+    idt_set_gate(43, (unsigned) irq10, 0x08, 0x8E);
+    idt_set_gate(44, (unsigned) irq11, 0x08, 0x8E);
+    idt_set_gate(45, (unsigned) irq12, 0x08, 0x8E);
+    idt_set_gate(46, (unsigned) irq13, 0x08, 0x8E);
+    idt_set_gate(47, (unsigned) irq14, 0x08, 0x8E);
 }
 
 void irq_handler(struct regs *r)
